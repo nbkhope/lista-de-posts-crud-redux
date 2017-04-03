8d050c94c46b7f50e1641d08fb02d4e72e573b18
nbkhope <ryumemaru@yahoo.com>
2017-04-02 20:03:36 -0700

    Add text inputs and OK button

--- src/components/PostNew.js
+++ src/components/PostNew.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { changePostTitle, changePostBody } from '../actions';
 
 class PostNew extends Component {
+  onTitleChange = (title) => {
+    console.log('Titulo modificado:', title);
+    this.props.changePostTitle(title);
+  }
+
+  onBodyChange = (body) => {
+    console.log('Conteudo modificado:', body);
+    this.props.changePostBody(body);
+  }
+
   render() {
     return (
       <View>
         <Text>Tudo legal? Novo post.</Text>
-        <Text>{this.props.title}</Text>
-        <Text>{this.props.body}</Text>
+        <Text style={{ fontWeight: 'bold' }}>Título</Text>
+        <TextInput
+          style={{ height: 40, borderWidth: 4 }}
+          onChangeText={this.onTitleChange}
+          value={this.props.title}
+        />
+
+        <Text style={{ fontWeight: 'bold' }}>Conteúdo</Text>
+        <TextInput
+          style={{ height: 80, borderWidth: 4 }}
+          onChangeText={this.onBodyChange}
+          value={this.props.body}
+          multiline={true}
+        />
+
+        <TouchableOpacity
+          style={{ borderWidth: 9, padding: 6, backgroundColor: 'antiquewhite' }}
+          onPress={() => null}
+        >
+          <Text>OK</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -19,8 +51,15 @@ const mapStateToProps = (state) => {
   return { title, body };
 };
 
-const mapDispatchToProps = () => {
-  return {};
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    changePostTitle,
+    changePostBody
+  }, dispatch);
 };
+
+// const mapDispatchToProps = {
+//   changePostTitle
+// };
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostNew);
