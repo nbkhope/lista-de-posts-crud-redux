6463e7551c2f046d866d9d5b7b19acbc5a35b854
nbkhope <ryumemaru@yahoo.com>
2017-06-11 22:38:27 -0700

    Handle UPDATE_POST, DELETE_POST, and CREATE_POST

--- src/reducers/PostsReducer.js
+++ src/reducers/PostsReducer.js
@@ -9,6 +9,25 @@ export default (state = INITIAL_STATE, action) => {
       return { ...state, all: action.payload.data };
     case 'FETCH_POST':
       return { ...state, selected: action.payload.data };
+    case 'UPDATE_POST': {
+      const all = state.all.map(post => {
+        if (post.id === action.payload.data.id) {
+          return action.payload.data;
+        }
+        else {
+          return post;
+        }
+      });
+      return { ...state, selected: action.payload.data, all };
+    }
+    case 'DELETE_POST': {
+      const all = state.all.filter(post => post.id != state.selected.id);
+      return { ...state, selected: null, all };
+    }
+    case 'CREATE_POST': {
+      const all = state.all.concat(action.payload.data);
+      return { ...state, all };
+    }
     default:
       return state;
   }
